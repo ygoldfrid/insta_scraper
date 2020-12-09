4ff48b99cdf0c44725fe92dc5150a3c77ddeea83
Dana Velibekov <danch22@gmail.com>
2020-12-09 14:09:28 +0200

    WiP api- exceptions, function split

--- weather_api.py
+++ weather_api.py
@@ -1,4 +1,3 @@
-import time
 import requests
 import config
 import api_keys
@@ -18,13 +17,12 @@ def google_geo_api(address):
         lat = geometry['location']['lat']
         lon = geometry['location']['lng']
     else:
-        return None, None
+        raise Exception("Unable to get location")
 
     return lat, lon
 
 
-def get_weather_api(address):
-    lat, lon = google_geo_api(address)
+def get_weather_api(lat, lon, address):
     params = {'lat': lat,
               'lon': lon,
               'appid': api_keys.API_KEY_WEATHER,
@@ -33,17 +31,17 @@ def get_weather_api(address):
 
     url = config.WEATHER
     res = requests.get(url, params=params).json()
-
-    temp = res['main']['temp']
-
-    latitude = res['coord']['lat']
-    longitude = res['coord']['lon']
-
-    description = res['weather'][0]['description']
-
-    return temp, latitude, longitude, description
+    if res['cod'] == 200:
+        temp = res['main']['temp']
+        latitude = res['coord']['lat']
+        longitude = res['coord']['lon']
+        description = res['weather'][0]['main']
+        return temp, latitude, longitude, description
+    else:
+        raise Exception("Unable to get weather details")
 
 
 test = r"Jerusalem, Israel"
-data = get_weather_api(address=test)
+lat, lon = google_geo_api(address=test)
+data = get_weather_api(lat=lat, lon=lon, address=test)
 print(data)
